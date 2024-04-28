707f680cc570b3ca9012d26cdab1a560f4fd8861
gdelgador <67815621+gdelgador@users.noreply.github.com>
2024-04-28 15:15:15 +0000

    hola.py modificado

--- Modulo1/hola.py
+++ Modulo1/hola.py
@@ -3,4 +3,5 @@
 # clear -> sobre terminal para limpiar mi terminal (en windows el comando se llama "cls")
 # cd -> cambiar directorio sobre terminal
 # python <nombre_archivo>.py -> para ejecutar mi programa python sobre la terminal
+
 print('hola mundo')
